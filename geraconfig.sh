a2fd7f5221eb5535917748760d6dd30380b5c5af
pedro-cavalcanti-de-carvalho <159674864+pedro-cavalcanti-de-carvalho@users.noreply.github.com>
2024-02-11 18:00:33 -0300

    Update geraconfig.sh

--- geraconfig.sh
+++ geraconfig.sh
@@ -1,16 +1,16 @@
 
-echo '{                                                     ' >> microservices.json
-echo '    "model": {                                        ' >> microservices.json
-echo '      "version": "V01",                               ' >> microservices.json
+echo '{                                                     ' >> MLOPS/MLOPS/microservices.json
+echo '    "model": {                                        ' >> MLOPS/MLOPS/microservices.json
+echo '      "version": "V01",                               ' >> MLOPS/MLOPS/microservices.json
 echo '      "url": "http://'$(sudo docker inspect servidor001 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
-['rede_trabalho']['IPAddress'])")':8080/predict"            ' >> microservices.json
-echo '    },                                                ' >> microservices.json
-echo '    "modelo_kmeans": {                                ' >> microservices.json
-echo '      "version": "V01",                               ' >> microservices.json
+['rede_trabalho']['IPAddress'])")':8080/predict"            ' >> MLOPS/MLOPS/microservices.json
+echo '    },                                                ' >> MLOPS/MLOPS/microservices.json
+echo '    "modelo_kmeans": {                                ' >> MLOPS/MLOPS/microservices.json
+echo '      "version": "V01",                               ' >> MLOPS/MLOPS/microservices.json
 echo '      "url": "http://'$(sudo docker inspect servidor002 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
-['rede_trabalho']['IPAddress'])")':8080/predict2"           ' >> microservices.json
-echo '    }                                                 ' >> microservices.json
-echo '}                                                     ' >> microservices.json
+['rede_trabalho']['IPAddress'])")':8080/predict2"           ' >> MLOPS/MLOPS/microservices.json
+echo '    }                                                 ' >> MLOPS/MLOPS/microservices.json
+echo '}                                                     ' >> MLOPS/MLOPS/microservices.json
 
 echo "Arquivo de configuração atualizado com sucesso. Veja seu conteúdo: "
 
