937b45157dc03ab17a43a06d088fc7c05a6c7a03
pedro-cavalcanti-de-carvalho <159674864+pedro-cavalcanti-de-carvalho@users.noreply.github.com>
2024-02-11 17:01:15 -0300

    geraconfig.sh

--- geraconfig.sh
+++ geraconfig.sh
@@ -1,11 +1,11 @@
 
 echo '{                                                     ' >  config/microservices.json
-echo '    "model": {                                     ' >> config/microservices.json
+echo '    "model": {                                        ' >> config/microservices.json
 echo '      "version": "V01",                               ' >> config/microservices.json
 echo '      "url": "http://'$(sudo docker inspect servidor001 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
 ['rede_trabalho']['IPAddress'])")':8080/predict"            ' >> config/microservices.json
 echo '    },                                                ' >> config/microservices.json
-echo '    "modelo_kmeans": {                                     ' >> config/microservices.json
+echo '    "modelo_kmeans": {                                ' >> config/microservices.json
 echo '      "version": "V01",                               ' >> config/microservices.json
 echo '      "url": "http://'$(sudo docker inspect servidor002 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
 ['rede_trabalho']['IPAddress'])")':8080/predict2"           ' >> config/microservices.json
