fcdc12e47195e317bd112fa9ed78bd0c9f45e3d0
pedro-cavalcanti-de-carvalho <159674864+pedro-cavalcanti-de-carvalho@users.noreply.github.com>
2024-02-11 17:57:26 -0300

    Update geraconfig.sh

--- geraconfig.sh
+++ geraconfig.sh
@@ -1,16 +1,16 @@
 
-echo '{                                                     ' >  config/microservices.json
-echo '    "model": {                                        ' >> config/microservices.json
-echo '      "version": "V01",                               ' >> config/microservices.json
+echo '{                                                     ' >> microservices.json
+echo '    "model": {                                        ' >> microservices.json
+echo '      "version": "V01",                               ' >> microservices.json
 echo '      "url": "http://'$(sudo docker inspect servidor001 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
-['rede_trabalho']['IPAddress'])")':8080/predict"            ' >> config/microservices.json
-echo '    },                                                ' >> config/microservices.json
-echo '    "modelo_kmeans": {                                ' >> config/microservices.json
-echo '      "version": "V01",                               ' >> config/microservices.json
+['rede_trabalho']['IPAddress'])")':8080/predict"            ' >> microservices.json
+echo '    },                                                ' >> microservices.json
+echo '    "modelo_kmeans": {                                ' >> microservices.json
+echo '      "version": "V01",                               ' >> microservices.json
 echo '      "url": "http://'$(sudo docker inspect servidor002 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
-['rede_trabalho']['IPAddress'])")':8080/predict2"           ' >> config/microservices.json
-echo '    }                                                 ' >> config/microservices.json
-echo '}                                                     ' >> config/microservices.json
+['rede_trabalho']['IPAddress'])")':8080/predict2"           ' >> microservices.json
+echo '    }                                                 ' >> microservices.json
+echo '}                                                     ' >> microservices.json
 
 echo "Arquivo de configuração atualizado com sucesso. Veja seu conteúdo: "
 
