f1f50b66b64616b75dc93e8e6a29cf647b6773c0
pedro-cavalcanti-de-carvalho <159674864+pedro-cavalcanti-de-carvalho@users.noreply.github.com>
2024-02-11 22:02:16 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,30 +1,28 @@
- import http.server
- import socketserver
- import pickle
- from http import HTTPStatus
+import http.server
+import socketserver
+import pickle
+from http import HTTPStatus
 
- class MyHandler(http.server.SimpleHTTPRequestHandler):
-     def do_POST(self):
-         content_length = int(self.headers['Content-Length'])
-         post_data = self.rfile.read(content_length)
-
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
          # Carregue o modelo aqui
-         model = pickle.load(open('model.pkl', 'rb'))
-
+        model = pickle.load(open('model.pkl', 'rb'))
          # Lógica de predição aqui (exemplo: sempre retorna 0)
          # Substitua essa lógica com a lógica real de predição
-         input_data = pickle.loads(post_data)
-         prediction = model.predict_proba([input_data])
+        input_data = pickle.loads(post_data)
+        prediction = model.predict_proba([input_data])
 
          # Envie a resposta para o cliente
-         self.send_response(HTTPStatus.OK)
-         self.send_header('Content-type', 'text/plain')
-         self.end_headers()
-         self.wfile.write(str(prediction).encode())
+        self.send_response(HTTPStatus.OK)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(str(prediction).encode())
 
- if __name__ == "__main__":
-     PORT = 8000
+if __name__ == "__main__":
+    PORT = 8000
 
-     with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-         print(f"Serving on port {PORT}")
-         httpd.serve_forever()
+    with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+        print(f"Serving on port {PORT}")
+        httpd.serve_forever()
